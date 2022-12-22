7a1cbf69e1160f520df0dce91d599cf046eb6058
SyedMohammed <syedmohdah@gmail.com>
2022-12-22 19:23:09 +0530

    Second Commit

--- ATM.py
+++ ATM.py
@@ -3,6 +3,7 @@
 #It is a Simulation of Bank ATM and you are having a balance of Rupees 50000
 import time
 print("This is Protptype of ATM System")
+balance=int(input("Enter an Amount to Deposit"))
 # Here You Can Choose The mode Of Transaction
 trans=str(input("Enter The mode of Transaction\nDr, Cr:\n")).capitalize()
 if trans=="Cr":
@@ -13,8 +14,9 @@ else:
     print("Invalid Mode of Transaction")
     exit()
 #You Can Enter The Amount of Money to be Withdrawn less than 50k
-num1=int(input("Enter the amount to be Withdraw:\n"))
-if num1>50000:
+withdraw=int(input("Enter the amount to be Withdraw:\n"))
+if withdraw>balance:
+    time.sleep(1)
     print("You Don't Have Sufficient funds!!!!!")
     exit()
 else:
@@ -31,7 +33,7 @@ print("Wait...")
 time.sleep(1)
 #If Your Password is Correct then Your Transaction will be succesfull
 print("Your Transaction Was Successful")
-balance=50000-num1
+balance=balance-withdraw
 time.sleep(2)
 #Here it will print the amount left after Transaction
 print(f"\nTotal Amount left is {balance}")
